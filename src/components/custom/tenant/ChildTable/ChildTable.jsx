63f08f5003ea328d42a06afad160909b0c5e7a8c
Ahmad Aktaa <ahmad.aktaa@roaa.tech>
2023-07-12 20:40:50 +0300

    ChildTable enhancement

--- src/components/custom/tenant/ChildTable/ChildTable.jsx
+++ src/components/custom/tenant/ChildTable/ChildTable.jsx
@@ -3,9 +3,10 @@ import React, { useState, useEffect } from "react";
 import { Card, Table } from "@themesberg/react-bootstrap";
 import { Wrapper } from "./ChildTable.style";
 import TenantStatus from "../TenantStatus/TenantStatus";
-import AccordionComponent from "../../../AccordionComponent";
+import AccordionComponent from "../../../AccordionComponent"; 
 
-export default function ChildTable() {
+
+export default function ChildTable() { 
   const rowExpansionTemplate = (data) => {
     return (
       <div className="">
@@ -46,16 +47,30 @@ export default function ChildTable() {
   ]);
 
   return (
-    <Wrapper>
-      <div className="card border-0">
-        <div className="status">
-          <span className="fw-bold statusTitle">status</span>
-          <span>
-            <TenantStatus statusValue={1} />
-          </span>
-        </div>
-        <AccordionComponent defaultKey="metaData" data={products} />
-      </div>
+    <Wrapper>    
+            <Card border="light" className="shadow-sm border-0">
+               <Card.Body className="p-0">
+                  <Table
+                  responsive
+                  className="table-centered table-nowrap rounded mb-0">
+                  <tbody>
+                    <tr>
+                      <td className="fw-bold">Status</td>
+                      <td> <TenantStatus statusValue={10} /></td>
+                    </tr>  
+                    <tr>
+                        <td className="fw-bold">
+                          Last Updated Date
+                        </td>
+                        <td>
+                        7/9/2023, 6:50:37 PM
+                        </td>
+                    </tr>
+                  </tbody>
+                </Table>
+              </Card.Body>
+            </Card>  
+            <AccordionComponent defaultKey="metaData" data={products} /> 
     </Wrapper>
   );
 }
