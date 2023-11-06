a56c309a88d01c7414ddd86dfe7757071db6ef77
bilal hkm <bilal.hkmsh@gmail.com>
2023-11-06 14:55:42 +0300

    add the random name for tenant

--- src/components/custom/tenant/TenantForm/TenantForm.styled.jsx
+++ src/components/custom/tenant/TenantForm/TenantForm.styled.jsx
@@ -1,2 +1,11 @@
 import styled from 'styled-components'
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+  .passive-toggle {
+    color: #ccc;
+    cursor: pointer;
+  }
+  .active-toggle {
+    color: var(--second-color);
+    cursor: pointer;
+  }
+`
