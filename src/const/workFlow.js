080b713ae84e872c50875f3eb320590db8c142ae
Ahmad Aktaa <ahmad.aktaa@roaa.tech>
2023-11-14 12:33:45 +0300

    processType updating

--- src/const/workFlow.js
+++ src/const/workFlow.js
@@ -16,7 +16,24 @@ export const processType = {
   8: 'Failed-To-Inform-External-System',
   9: 'Payment-Status-Suspended-Due-To-Non-Renewal',
   10: 'Specifications-Updated',
-  11: 'Subscription-Reset',
+  /*11: 'Subscription-Reset',
   12: 'Subscription-Reset-Failure',
-  13: 'Subscription-Feature-Limit-Reset',
+  13: 'Subscription-Feature-Limit-Reset',*/ 
+  11:'Subscription-Reset-Prepared',
+  12:'Subscription-Reset-Applied-Done',
+  13:'Subscription-Reset-Application-Failed',
+  14:'Subscription-Feature-Limit-Reset',
+  15:'Subscription-Auto-Renewal-Enabled',
+  16:'Subscription-Auto-Renewal-Canceled',
+  17:'Subscription-Renewed',
+  18:'Subscription-Upgrade-Prepared', // تم التحضير لعملية ترقية الاشتراك
+  19:'Subscription-Upgraded',// تم ترقية خطة الاشتراك في نظام رصاص 
+  20:'Subscription-Upgrade-Being-Applied',//جارٍ تطبيق ترقية الاشتراك 
+  21:'Subscription-Upgrade-Application-Failed', // فشل عملية تطبيق ترقية الاشتراك في النظام الخارجي
+  22:'Subscription-Upgrade-Applied-Done',//تم تطبيق ترقية الاشتراك 
+  23:'Subscription-Downgrade-Prepared',
+  24:'Subscription-Downgraded',
+  25:'Subscription-Downgrade-Being-Applied',
+  26:'Subscription-Downgrade-Application-Failed',
+  27:'Subscription-Downgrade-Applied-Done',
 }
