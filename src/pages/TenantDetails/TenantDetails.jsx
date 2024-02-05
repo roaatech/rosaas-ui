b06538a0e4388d1c1bc7e8dda465b64bf845100b
bilal hkm <bilal.hkmsh@gmail.com>
2024-02-05 12:57:46 +0300

    enhancements

--- src/pages/TenantDetails/TenantDetails.jsx
+++ src/pages/TenantDetails/TenantDetails.jsx
@@ -89,7 +89,7 @@ const TenantDetails = () => {
 
   tenantObject?.subscriptions.map((item, index) => {
     if (firstLoad == 0 && item?.name == window.location.href.split('#')[1]) {
-      dispatch(setActiveIndex(index + 1))
+      dispatch(setActiveIndex(index + 2))
       firstLoad++
     }
   })
