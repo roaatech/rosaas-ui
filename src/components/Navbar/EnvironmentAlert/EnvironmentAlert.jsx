89e343d501dafaf9800476763bec820cf38a4adb
bilal hkm <bilal.hkmsh@gmail.com>
2024-09-16 14:36:11 +0300

    environment header bar alert

--- src/components/Navbar/EnvironmentAlert/EnvironmentAlert.jsx
+++ src/components/Navbar/EnvironmentAlert/EnvironmentAlert.jsx
@@ -2,12 +2,21 @@ import React, { useEffect, useState } from 'react'
 import { Alert } from '@themesberg/react-bootstrap'
 import useRequest from '../../../axios/apis/useRequest'
 import { lowerCase } from 'lodash'
-import { useDispatch } from 'react-redux' // Import dispatch
-import { setEnvironmentAlertData } from '../../../store/slices/main' // Import the action
+import { useDispatch } from 'react-redux'
+import { setEnvironmentAlertData } from '../../../store/slices/main'
+import {
+  FaExclamationTriangle,
+  FaInfoCircle,
+  FaCheckCircle,
+  FaCogs,
+  FaHome,
+} from 'react-icons/fa' // Icons for different environments
+import Label from '../../custom/Shared/label/Label'
 
 const EnvironmentAlert = () => {
   const { getEnvironment } = useRequest()
   const [environmentData, setEnvironmentData] = useState(null)
+  const [showAlert, setShowAlert] = useState(true) // State to control alert visibility
   const dispatch = useDispatch() // Initialize dispatch
 
   const currentUrl = window.location.href // Get the current URL
@@ -51,6 +60,48 @@ const EnvironmentAlert = () => {
     }
   }
 
+  const getLabelDetails = (env) => {
+    const lowerCasedEnv = lowerCase(env)
+    switch (lowerCasedEnv) {
+      case 'development':
+        return {
+          color: '#856404',
+          background: '#fff3cd',
+          value: 'Development',
+        }
+      case 'stage':
+        return {
+          color: '#0c5460',
+          background: '#d1ecf1',
+          value: 'Stage',
+        }
+      case 'production':
+        return {
+          color: '#155724',
+          background: '#d4edda',
+          value: 'Production',
+        }
+      case 'sandbox':
+        return {
+          color: '#383d41',
+          background: '#e2e3e5',
+          value: 'Sandbox',
+        }
+      case 'localhost':
+        return {
+          color: '#004085',
+          background: '#cce5ff',
+          value: 'Localhost',
+        }
+      default:
+        return {
+          color: '#004085',
+          background: '#cce5ff',
+          value: 'Unknown',
+        }
+    }
+  }
+
   useEffect(() => {
     if (!currentUrl || !environmentFromEnv) {
       return
@@ -80,30 +131,11 @@ const EnvironmentAlert = () => {
     fetchEnvironmentData()
   }, [currentUrl, environmentFromEnv])
 
-  const getVariant = (env) => {
-    const lowerCasedEnv = lowerCase(env)
-
-    switch (lowerCasedEnv) {
-      case 'development':
-        return 'warning'
-      case 'stage':
-        return 'info'
-      case 'production':
-        return 'success'
-      case 'sandbox':
-        return 'secondary'
-      case 'localhost':
-        return 'primary'
-      default:
-        return 'primary'
-    }
-  }
-
   if (environmentFromEnv === 'production') {
-    return null // Do not render the alert in production
+    return null
   }
 
-  if (!environmentData) {
+  if (!environmentData || !showAlert) {
     return null
   }
 
@@ -113,28 +145,41 @@ const EnvironmentAlert = () => {
     lowerCase(requestDomain)
   )
 
-  const alertVariant = getVariant(currentEnvironment || environmentFromEnv)
-
   return (
-    <Alert variant={alertVariant} style={{ width: '100% !important' }}>
+    <Alert
+      variant={getLabelDetails(currentEnvironment).variant}
+      dismissible
+      onClose={() => setShowAlert(false)} // Handle closing of the alert
+      style={{
+        position: 'sticky',
+        top: 0,
+        left: 0,
+        width: '100%',
+        backgroundColor: 'var(--second-color-1)',
+        zIndex: 1000,
+        borderRadius: 0,
+        border: 'none',
+      }}
+    >
       <div className="d-flex flex-row justify-content-around">
         <div>
-          <strong>Environment from Request:</strong>{' '}
-          {environmentFromRequest || 'Not available'}
+          <strong>Dashboard:</strong>
+          <Label className={'mx-2'} {...getLabelDetails(domain)} />
         </div>
-
         <div>
-          <strong>Environment from process.env:</strong>{' '}
-          {environmentFromEnv || 'Not available'}
+          <strong>NODE ENVIRONMENT:</strong>
+          <Label className={'mx-2'} {...getLabelDetails(environmentFromEnv)} />
         </div>
-
         <div>
-          <strong>Current URL:</strong> {domain}
+          <strong>API ENVIRONMENT:</strong>
+          <Label
+            className={'mx-2'}
+            {...getLabelDetails(environmentFromRequest)}
+          />
         </div>
-
         <div>
-          {' '}
-          <strong>Request Domain:</strong> {requestEnvironment}
+          <strong>API URL:</strong>
+          <Label className={'mx-2'} {...getLabelDetails(requestEnvironment)} />
         </div>
       </div>
     </Alert>
