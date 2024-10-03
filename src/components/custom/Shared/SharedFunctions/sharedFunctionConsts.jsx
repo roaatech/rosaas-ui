a5c9e534dc9b01e54ff112ab90892d5c70728a2d
bilal hkm <bilal.hkmsh@gmail.com>
2024-10-03 14:37:29 +0300

    solve bug of two requests in audits

--- src/components/custom/Shared/SharedFunctions/sharedFunctionConsts.jsx
+++ src/components/custom/Shared/SharedFunctions/sharedFunctionConsts.jsx
@@ -1,7 +1,7 @@
 export const arraysEqual = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) return false
+  if (arr1 && arr1.length !== arr2 && arr2.length) return false
 
-  for (let i = 0; i < arr1.length; i++) {
+  for (let i = 0; i < arr1 && arr1.length; i++) {
     if (arr1[i] !== arr2[i]) return false
   }
 
